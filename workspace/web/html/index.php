a8710cd62a2e7ca68989b53649498032a3058c51
Chris Roberton <robertonchristopher@gmail.com>
2015-04-08 20:42:45 -0600

    All league schedules added through WordPress tables

--- workspace/web/html/index.php
+++ workspace/web/html/index.php
@@ -69,11 +69,7 @@ require('./blog/wp-blog-header.php');
 	<script type="text/javascript">
 		$(document).ready(function() {
 		
-			$("#schedule_accordion").accordion(
-				{
-					"heightStyle": "content",
-				}
-			);
+			
 			
 			$("#tablepress-1").DataTable( {
 				"columns": [
@@ -99,10 +95,17 @@ require('./blog/wp-blog-header.php');
         			"aaSorting": [],
         			"lengthChange": false,
         			"bSort" : false,
-  					"stripeClasses": [ 'strip1', 'strip2', 'strip3' ]
+  					"stripeClasses": [ 'strip1', 'strip2'],
+  					"paging":false,
+  					"searching":false,
+  					"info":false
 				}); 
 				
-				
+			$("#schedule_accordion").accordion(
+				{
+					"heightStyle": "content",
+				}
+			);	
 				
 				
 			$("#contact_form_send").click(function() {
@@ -261,132 +264,22 @@ require('./blog/wp-blog-header.php');
 			</p>
 			<hr>
 			<div id="schedule_accordion">			
-				<?php
-				
-	error_reporting(E_ALL ^ E_DEPRECATED);
-
-	$username="strongsi_brian";
-	$password="124578";
-	$database="strongsi_vball_data";
-	
-	mysql_connect("localhost",$username,$password);
-	
-	@mysql_select_db($database) or die( "Unable to select database");
-
-	//Determine the leagues
-	$leagues_query="select distinct league_id from games";
-	$leagues_result=mysql_query($leagues_query);
-	
-	$league_iter = 0;
-	$num_leagues = mysql_numrows($leagues_result);
-	
-	while ($league_iter < $num_leagues) {
-		
-		$league_id = mysql_result($leagues_result,$league_iter,"league_id");
-		
-		//Determine the name of the league by id
-		$league_name_query = "select gender_format, night from leagues where id = " . $league_id;
-		$league_name_result = mysql_query($league_name_query);
-		$league_name = mysql_result($league_name_result,0,"gender_format");
-		$league_night = mysql_result($league_name_result,0,"night");
-	
-		echo "<h3 class=\"accordion_section_header\">" . $league_night . " " . $league_name . "</h3>";		
-		?>
-		<div>
-		<p>
-		<table class="schedule_table" id='<?php echo "league_" . $league_id;?>'>
-		<thead>
-		<tr><th>&nbsp;</th>
-		<?php
-		
-		//Determine the locations
-		$locations_query = "select distinct location from games where league_id = " . $league_id;
-		$locations_result = mysql_query($locations_query);
-	
-		//Determine the times
-		$times_query = "select distinct time from games where league_id = " . $league_id;
-		$times_result = mysql_query($times_query);
-		
-		//Print the table column headers
-		$time_iter = 0;
-		$num_locations = mysql_numrows($locations_result);
-		$num_times = mysql_numrows($times_result);
-		
-		while ($time_iter < $num_times) {
-			
-			$location_iter = 0;
-
-			 while ($location_iter < $num_locations) {				
-				
-			 	echo "<th>" . mysql_result($times_result, $time_iter, "time") . "<br/>" . mysql_result($locations_result, $location_iter, "location") . "</th>";
-		
-				$location_iter++;
-			}
-		
-			$time_iter++;
-		}
-		
-		echo "</tr></thead>";
-		
-		echo "<tbody>";
-		//select distinct weeks per league
-		$weeks_query = "select distinct week from games where league_id = " . $league_id;
-		$weeks_result = mysql_query($weeks_query);
-		
-		//select distinct dates per league
-		$dates_query = "select distinct date from games where league_id = " . $league_id;
-		$dates_result = mysql_query($dates_query);
-		
-		//iterate over the number of weeks (we have to assume there is a 1-1 relationship between weeks and dates in any given league
-		$week_iter = 0;
-		$num_weeks = mysql_numrows($weeks_result);
-		
-		while ($week_iter < $num_weeks) {
-		
-			$week = mysql_result($weeks_result, $week_iter, "week");
-			
-			echo "<tr><td>Week " . $week  . "<br/>";
-			echo mysql_result($dates_result, $week_iter, "date");
-			echo "</td>";
-		
-			$time_iter = 0;
-			while ($time_iter < $num_times) {
-			
-				$location_iter = 0;
-				
-				 while ($location_iter < $num_locations) {
-				
-					$game_location = mysql_result($locations_result, $location_iter, "location");
-					$game_time = mysql_result($times_result, $time_iter, "time");
-					
-					$game_query = "select home_team, away_team, score from games where league_id = " . $league_id . " and week = " . $week . " and location = \"" . $game_location . "\"" . " and time = \"" . $game_time . "\";"; 
-					$game_result = mysql_query($game_query);
-					
-					if (mysql_numrows($game_result) == 1) {
-						$home_team = mysql_result($game_result, 0, "home_team");
-						$away_team = mysql_result($game_result, 0, "away_team");
-						$score = mysql_result($game_result, 0, "score");
-					
-						echo "<td>" . $home_team . " v " . $away_team . "<br/>" . $score . "</td>";
-					} else {
-						echo "<td>-</td>";
-					} 
-			
-					$location_iter++;
-				}
-				
-				$time_iter++;
-			}
-		
-			echo "</tr>";
-			$week_iter++;
-		}
-		
-		echo "</tbody></table></p></div>";
-		
-		$league_iter++;
-	}
-?>
+				<h3 class="accordion_section_header">Tuesday Coed Fours</h3>
+				<?php tablepress_print_table( 'id=2&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Tuesday Coed Sixes</h3>
+				<?php tablepress_print_table( 'id=3&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Wednesday Coed Fours</h3>
+				<?php tablepress_print_table( 'id=4&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Wednesday Coed Sixes</h3>
+				<?php tablepress_print_table( 'id=5&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Thursday Coed Fours</h3>
+				<?php tablepress_print_table( 'id=6&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Thursday Coed Sixes</h3>
+				<?php tablepress_print_table( 'id=7&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Sunday Mens Doubles</h3>
+				<?php tablepress_print_table( 'id=8&use_datatables=true&print_name=false' ); ?>
+				<h3 class="accordion_section_header">Sunday Womens Doubles</h3>
+				<?php tablepress_print_table( 'id=9&use_datatables=true&print_name=false' ); ?>
 			</div>
 		</div>         	
 	</div>	
